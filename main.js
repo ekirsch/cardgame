071bdbf91287b504c75f6adf1233765a66a73323
ekirsch <33180382+ekirsch@users.noreply.github.com>
2017-10-28 11:09:27 -0400

    fixing error

--- main.js
+++ main.js
@@ -38,6 +38,40 @@ cardContDiv.appendChild(flipDiv)
 var cardDeck = document.getElementById("cardDeck")
 cardDeck.appendChild(cardContDiv)
 cardContDiv.onclick = function(e){
+    console.log(e.currentTarget)
+    setTimeout(function){ e.currentTarget.classList.toggle("flip_card")})
+
+     e.currentTarget.classList.toggle("flip_card")
+            
+}
+
+for (var i = cardNumbers.length - 1; i >= 0; i--){
+    
+    for (var j = cardSuit.length - 1; j >= 0; j--){
+         var CardDeck = new Card();
+            CardDeck.init();
+        CardDeck.number.innerHTML = cardNumbers[i]
+        CardDeck.suit.src = "images/" + cardSuit[j] + ".svg";
+        
+    }
+}
+
+
+
+
+//var CardDeck = new Card();
+//CardDeck.init();
+//
+//var CardDeck2 = new Card();
+//CardDeck2.init();
+//CardDeck2.suit.src = "images/spades.svg"
+//
+//var CardDeck3 = new Card();
+//CardDeck3.init();
+//CardDeck3.suit.src = "images/diamonds.svg"
+
+
+
             console.log(e.currentTarget)
     setTimeout(function){ e.currentTarget.classList.toggle("flip_card")
         
